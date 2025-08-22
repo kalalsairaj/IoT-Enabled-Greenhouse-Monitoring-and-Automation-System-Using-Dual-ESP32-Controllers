a65c0247b8add29ef0dfc9e5c27c38fd6c0ecd60
Sairaj Kalal <142725727+kalalsairaj@users.noreply.github.com>
2025-08-22 16:54:24 +0530

    Create esp32-sensor.ino

---
+++ code/esp32-sensor.ino
@@ -0,0 +1,71 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+
+// ==== Wi-Fi Settings ====
+const char* ssid = "V2029";
+const char* password = "15092004";
+
+// ==== MQTT Settings ====
+const char* mqtt_server = "test.mosquitto.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// ==== DHT Settings ====
+#define DHTPIN 4
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+// ==== Soil Sensor ====
+#define SOIL_PIN 34 // Analog input
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  client.setServer(mqtt_server, 1883);
+  dht.begin();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Read sensors
+  float temp = dht.readTemperature();
+  int rawSoil = analogRead(SOIL_PIN);
+  int soilValue = 4095 - rawSoil;
+
+  // Create payload
+  String payload = String(temp) + "," + String(soilValue);
+  
+  // Publish
+  client.publish("greenhouse/data", payload.c_str());
+  Serial.println("Sent: " + payload);
+
+  delay(5000);
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP32-Sensor")) {
+      Serial.println("MQTT connected");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 2s");
+      delay(2000);
+    }
+  }
+}
